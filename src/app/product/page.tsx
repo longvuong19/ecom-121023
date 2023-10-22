18dec45f200db1174aa45b7644f1349cfcb9f29a
LongVuong <longvuong1920@gmail.com>
2023-10-22 22:57:11 +0700

    add product card

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import Container from "@/components/Container";
+import { getSingleProduct, getTrendingProducts } from "@/helpers";
+import { Products } from "../../../type";
+import ProductsData from "@/components/ProductsData";
+import SingleProduct from "@/components/SingleProduct";
 
-const ProductPage = () => {
-  return <div>ProductPage</div>;
+type Props = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+const ProductPage = async ({ searchParams }: Props) => {
+  const _idString = searchParams?._id;
+  const _id = Number(_idString);
+  const product = getSingleProduct(_id);
+  const data = await getTrendingProducts();
+
+  return (
+    <div>
+      <Container>
+        <SingleProduct product={product} />
+        <div>
+          <p className="text-xl py-1 font-bold">Trending Products</p>
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+            {data.map((item: Products) => (
+              <ProductsData key={item._id} item={item} />
+            ))}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default ProductPage;
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,5 @@
+import { productData } from "@/constants/data";
+
 export const getProducts = async () => {
   const res = await fetch("https://fakestoreapiserver.reactbd.com/smart");
   if (!res.ok) throw new Error("Failed to load products");
@@ -9,4 +11,18 @@ export const calculatePercentage = (oldPrice: any, price: any) => {
   return !!parseFloat(price) && !!parseFloat(oldPrice)
     ? (100 - (oldPrice / price) * 100).toFixed(0)
     : 0;
+};
+
+export const getSingleProduct = (_id: number) => {
+  const item = productData.find((product) => product._id === _id);
+  return item;
+};
+
+export const getTrendingProducts = async () => {
+  const res = await fetch(
+    "https://fakestoreapiserver.reactbd.com/smarttrending"
+  );
+  if (!res.ok) throw new Error("Failed to load products");
+
+  return res.json();
 };
